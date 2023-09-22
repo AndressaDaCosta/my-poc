04f827c978eadef3dac0dcb3dc0b9f5b9d21612c
Andressa Da Costa <andressadacostan@gmail.com>
2023-09-22 11:03:23 -0300

    Next-image-otimization

--- README.md
+++ README.md
@@ -1,16 +1,51 @@
-# Proof of Concept (PoC) - Indexação
+# Proof of Concept (PoC) - Indexação e carregamento de Imagens
 
-Esta é uma prova de conceito simples que demonstra como criar várias páginas no Next.js e gerenciar metadados (como título, descrição e ícone) para cada página.
-E também gerenciar os dados compartilhados através de whatsapp com open graph.
+Esta é uma prova de conceito simples que demonstra como criar várias páginas no Next.js e gerenciar metadados (como título, descrição e ícone) para cada página, a gerenciar os dados compartilhados através de whatsapp com open graph.
+E também como usar o Next.js para otimizar o carregamento de imagens em uma aplicação web. A PoC inclui uma comparação entre o carregamento de imagens diretamente do HTML e o carregamento de imagens usando o componente Next.js Image Optimization.
 
 ## Estrutura de Pastas
 
 -   `pages/`: Contém as páginas da aplicação, incluindo a página inicial e as páginas de layout.
 -   `public/`: Armazena arquivos estáticos, como ícones.
--   `components/`: Contém os componentes reutilizáveis, como o layout.
+-   `components/`: Contém os componentes reutilizáveis, como o Layout e NextImage
 -   `metadata.json`: Um arquivo JSON que armazena os metadados para cada layout.
 
-## Como Rodar
+## Metadados e Layouts
+
+-   A PoC inclui várias páginas de layout (layout1, layout2, layout3, layout4).
+-   Cada página de layout possui seu próprio título, descrição e ícone definidos no arquivo `metadata.json`.
+-   O ícone de cada página esta armazenado na pasta `public/`.
+
+### Imagens Não Otimizadas
+
+No HTML padrão, as imagens são carregadas diretamente com a tag `<img>`. No entanto, isso requer otimizações manuais, como dimensionamento, carregamento preguiçoso e otimização de formato.
+
+### Otimização de Imagens com o Componente Image do Next.js
+
+O Next.js fornece o componente `Image`, que lida com essas otimizações automaticamente. Ele oferece as seguintes vantagens:
+
+-   Redimensionamento automático das imagens com base nas necessidades.
+-   Carregamento lazy-loading das imagens, economizando largura de banda e acelerando o carregamento da página.
+-   Otimização automática do formato da imagem com base no suporte do navegador.
+
+### Comparação de Desempenho
+
+Imagem carregada diretamente do HTML:
+Tempo de carregamento: 168ms
+
+Imagem carregada usando o componente Image do Next.js:
+Tempo de carregamento: 82ms
+
+<img src="./public/assets/time-images.png" >
+<img src="./public/assets/imagem-next-html.png" >
+
+### Tecnologias Usadas
+
+-   Next.js
+-   React
+-   HTML/CSS
+
+### Como Rodar
 
 1. Certifique-se de ter o Node.js instalado em seu sistema.
 2. Clone este repositório para sua máquina local usando `git clone`.
@@ -18,18 +53,6 @@ E também gerenciar os dados compartilhados através de whatsapp com open graph.
 4. Instale as dependências com `npm install`.
 5. Inicie o servidor de desenvolvimento com `npm run dev`.
 6. Abra seu navegador e acesse `http://localhost:3000` para ver a PoC em funcionamento.
-
-## Metadados e Layouts
-
--   A PoC inclui várias páginas de layout (layout1, layout2, layout3, layout4).
--   Cada página de layout possui seu próprio título, descrição e ícone definidos no arquivo `metadata.json`.
--   O ícone de cada página deve ser armazenado na pasta `public/`.
-
-## Tecnologias Usadas
-
--   Next.js
--   React
--   HTML/CSS
 
 #
 
--- src/pages/layout1.tsx
+++ src/pages/layout1.tsx
@@ -1,49 +1,87 @@
-
 // pages/layout1.tsx
-import React, { useEffect, useState } from 'react';
-import Head from 'next/head';
+import React, { useEffect, useState } from "react"
+import Head from "next/head"
+import NextImage from "./components/NextImage"
 // import layoutData from '../../public/metadata.json';
+import ImagemPesadaSvg from "../../assets/imagem-pesada.svg"
 
 export default function Layout1() {
-    // const data = layoutData.layout1;
-    const [data, setData] = useState({
-        title: '',
-        description: '',
-        image: '',
-      });
-    
-      useEffect(() => {
-        // Simulação de uma solicitação fetch para buscar dados do arquivo JSON
-        fetch('/metadata.json')
-          .then((response) => response.json())
-          .then((jsonData) => {
-            const layoutData = jsonData.layout1;
-            setData({
-              title: layoutData.title,
-              description: layoutData.description,
-              image: layoutData.image,
-            });
-          })
-          .catch((error) => {
-            console.error('Erro ao buscar dados:', error);
-          });
-      }, []);
-    
-  return (
-    <div>
-        <Head>
-        <title>{data.title}</title>
-        <link rel="icon" href={data.image} /> 
-        
-        {/* Meta tags OG para compartilhamento em redes sociais como facebook e whatsapp*/}
-        <meta property="og:title" content={data.title} />
-        <meta property="og:description" content={data.description} />
-        <meta property="og:image" content={data.image} />
-        <meta property="og:url" content={`www.${data.title}-exemplo.com.br`} />
-        <meta property="og:type" content="website" />
-      </Head>
-      <h1> Layout 1</h1>
-      <img  src={data.image} alt={`Ícone de ${data.title}`} />
-    </div>
-  );
+	// const data = layoutData.layout1;
+	const [data, setData] = useState({
+		title: "",
+		description: "",
+		image: ""
+	})
+
+	useEffect(() => {
+		// Simulação de uma solicitação fetch para buscar dados do arquivo JSON
+		fetch("/metadata.json")
+			.then((response) => response.json())
+			.then((jsonData) => {
+				const layoutData = jsonData.layout1
+				setData({
+					title: layoutData.title,
+					description: layoutData.description,
+					image: layoutData.image
+				})
+			})
+			.catch((error) => {
+				console.error("Erro ao buscar dados:", error)
+			})
+	}, [])
+
+	return (
+		<div>
+			<Head>
+				<title>{data.title}</title>
+				<link
+					rel="icon"
+					href={data.image}
+				/>
+
+				{/* Meta tags OG para compartilhamento em redes sociais como facebook e whatsapp*/}
+				<meta
+					property="og:title"
+					content={data.title}
+				/>
+				<meta
+					property="og:description"
+					content={data.description}
+				/>
+				<meta
+					property="og:image"
+					content={data.image}
+				/>
+				<meta
+					property="og:url"
+					content={`www.${data.title}-exemplo.com.br`}
+				/>
+				<meta
+					property="og:type"
+					content="website"
+				/>
+			</Head>
+			<img
+				title="favicon"
+				src={data.image}
+				alt={`Ícone de ${data.title}`}
+			/>
+			<h1> Layout 1</h1>
+
+			<div style={{ display: "flex", alignItems: "center" }}>
+				<div style={{ textAlign: "center" }}>
+					<NextImage />
+				</div>
+				<div style={{ textAlign: "center" }}>
+					<h2>Imagem HTML</h2>
+					<img
+						src="/assets/imagem-pesada.svg"
+						width={300}
+						height={100}
+						alt="imagem img"
+					/>
+				</div>
+			</div>
+		</div>
+	)
 }
