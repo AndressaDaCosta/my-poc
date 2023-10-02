d041a66887fb3a2ea365718f033ca25a56683d6b
Matheus de Souza <matheusrodrigues@poli.ufrj.br>
2023-10-02 17:06:06 -0300

    fix: storing event in state variable

--- app/components/SiteComponent.tsx
+++ app/components/SiteComponent.tsx
@@ -1,126 +1,140 @@
-"use client"
-import { useEffect, useState } from "react"
-import styles from "../styles/SiteComponent.module.css"
+"use client";
+import { useEffect, useState } from "react";
+import styles from "../styles/SiteComponent.module.css";
 
 type SiteConfig = {
-	data: {
-		domain: string
-		theme: {
-			id: number
-		}
-		storeType: string
-		sections: {
-			configurations: {
-				title: string
-				favicon: string
-				description: string
-			}
-		}
-	}
-}
+  data: {
+    domain: string;
+    theme: {
+      id: number;
+    };
+    storeType: string;
+    sections: {
+      configurations: {
+        title: string;
+        favicon: string;
+        description: string;
+      };
+    };
+  };
+};
 type InstallPromptResult = {
-	outcome: "accepted" | "dismissed"
-	platform: string
-}
+  outcome: "accepted" | "dismissed";
+  platform: string;
+};
 
 export default function SiteComponent() {
-	const [siteConfig, setSiteConfig] = useState<SiteConfig | null>(null)
-	const [setupButtonVisible, setSetupButtonVisible] = useState(false)
+  const [siteConfig, setSiteConfig] = useState<SiteConfig | null>(null);
+  const [setupButtonVisible, setSetupButtonVisible] = useState(false);
+  const [deferredPrompt, setDeferredPrompt] = useState<any>(null);
 
-	useEffect(() => {
-		fetch(`https://api-site-config.convem.me/V1/config-json/539`)
-			.then((response) => {
-				if (!response.ok) {
-					throw new Error("Erro na solicitação à API")
-				}
-				return response.json()
-			})
-			.then((data: SiteConfig) => {
-				setSiteConfig(data)
-			})
-			.catch((error) => {
-				console.error(error)
-				setSiteConfig(null)
-			})
-		setSetupButtonVisible(true)
-	}, [])
+  useEffect(() => {
+    fetch(`https://api-site-config.convem.me/V1/config-json/539`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Erro na solicitação à API");
+        }
+        return response.json();
+      })
+      .then((data: SiteConfig) => {
+        setSiteConfig(data);
+      })
+      .catch((error) => {
+        console.error(error);
+        setSiteConfig(null);
+      });
+    setSetupButtonVisible(true);
+  }, []);
 
-	//    @ts-ignore
-	let deferredPrompt: BeforeInstallPromptEvent | null = null
-	// @ts-ignore
-	const handleBeforeInstallPrompt = (e: BeforeInstallPromptEvent) => {
-		e.preventDefault()
-		deferredPrompt = e
-	}
+  window.addEventListener("beforeinstallprompt", (e) => {
+    e.preventDefault();
+    setDeferredPrompt(e);
+  });
 
-	useEffect(() => {
-		window.addEventListener(
-			"beforeinstallprompt",
-			handleBeforeInstallPrompt
-		)
+  const installApp = async () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+      const { outcome } = deferredPrompt.userChoice.then(
+        (choiceResult: InstallPromptResult) => {
+          if (choiceResult.outcome === "accepted") {
+            console.log("PWA configurado com sucesso");
+          } else {
+            console.log("Configuração do PWA rejeitada");
+          }
+          setDeferredPrompt(null);
+        },
+      );
+    }
+  };
 
-		return () => {
-			window.removeEventListener(
-				"beforeinstallprompt",
-				handleBeforeInstallPrompt
-			)
-		}
-	}, [])
+  // const handleBeforeInstallPrompt = (e: any) => {
+  //   e.preventDefault();
+  //   deferredPrompt = e;
+  // };
+  //
+  // useEffect(() => {
+  //   window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+  //
+  //   // return () => {
+  //   // 	window.removeEventListener(
+  //   // 		"beforeinstallprompt",
+  //   // 		handleBeforeInstallPrompt
+  //   // 	)
+  //   // }
+  // }, []);
+  //
+  // const installApp = () => {
+  //   if (deferredPrompt) {
+  //     deferredPrompt.prompt();
+  //     setSetupButtonVisible(false);
+  //
+  //     deferredPrompt.userChoice.then((choiceResult: InstallPromptResult) => {
+  //       if (choiceResult.outcome === "accepted") {
+  //         console.log("PWA configurado com sucesso");
+  //       } else {
+  //         console.log("Configuração do PWA rejeitada");
+  //       }
+  //
+  //       deferredPrompt = null;
+  //     });
+  //   }
+  // };
 
-	const installApp = () => {
-		if (deferredPrompt) {
-			deferredPrompt.prompt()
-			setSetupButtonVisible(false)
-
-			deferredPrompt.userChoice.then(
-				(choiceResult: InstallPromptResult) => {
-					if (choiceResult.outcome === "accepted") {
-						console.log("PWA configurado com sucesso")
-					} else {
-						console.log("Configuração do PWA rejeitada")
-					}
-
-					deferredPrompt = null
-				}
-			)
-		}
-	}
-
-	if (!siteConfig) {
-		return <div>Carregando...</div>
-	}
-	return (
-		<div className={`${styles.container} ${styles.cardContainer}`}>
-			<div className={styles.cardInfo}>
-				<p>
-					<strong>Domínio:</strong> {siteConfig.data.domain}
-				</p>
-				<p>
-					<strong>Tipo de Site:</strong> {siteConfig.data.storeType}
-				</p>
-				<p>
-					<strong>Id Layout:</strong> {siteConfig.data.theme?.id}
-				</p>
-				<p>
-					<strong>Titulo:</strong>{" "}
-					{siteConfig.data.sections.configurations?.title}
-				</p>
-				<p>
-					<strong>Favicon: </strong>
-					<img
-						src={siteConfig.data.sections.configurations?.favicon}
-						alt="Favicon"
-						width={40}
-					/>
-				</p>
-				<p>
-					<strong> Descrição: </strong>
-					{siteConfig.data.sections.configurations?.description}
-				</p>
-				{setupButtonVisible && (
-					<button onClick={installApp}>Baixar o App</button>
-				)}
-			</div>
-		</div>
-	)
+  if (!siteConfig) {
+    return <div>Carregando...</div>;
+  }
+  return (
+    <div className={`${styles.container} ${styles.cardContainer}`}>
+      <div className={styles.cardInfo}>
+        <p>
+          <strong>Domínio:</strong> {siteConfig.data.domain}
+        </p>
+        <p>
+          <strong>Tipo de Site:</strong> {siteConfig.data.storeType}
+        </p>
+        <p>
+          <strong>Id Layout:</strong> {siteConfig.data.theme?.id}
+        </p>
+        <p>
+          <strong>Titulo:</strong>{" "}
+          {siteConfig.data.sections.configurations?.title}
+        </p>
+        <p>
+          <strong>Favicon: </strong>
+          <img
+            src={siteConfig.data.sections.configurations?.favicon}
+            alt="Favicon"
+            width={40}
+          />
+        </p>
+        <p>
+          <strong> Descrição: </strong>
+          {siteConfig.data.sections.configurations?.description}
+        </p>
+        {setupButtonVisible && (
+          <button onClick={installApp}>Baixar o App</button>
+        )}
+      </div>
+    </div>
+  );
 }
